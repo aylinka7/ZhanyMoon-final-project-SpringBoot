8d0986ac299a2eef8afded73be3e827b9a60676f
aylinka7 <ayladylova@gmail.com>
2025-12-16 17:18:05 +0600

    telegram bot 2

--- src/main/java/org/example/zhanymoon/service/MyTelegramBot.java
+++ src/main/java/org/example/zhanymoon/service/MyTelegramBot.java
@@ -8,10 +8,10 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 @Component
 public class MyTelegramBot extends TelegramLongPollingBot {
 
-  @Value("${telegram.bot-username}")
+  @Value("${telegram.bot.username}")
   private String botUsername;
 
-  @Value("${telegram.bot-token}")
+  @Value("${telegram.bot.token}")
   private String botToken;
 
   @Override
